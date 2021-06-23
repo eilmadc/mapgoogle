dfafe95092616573c2f332360f18a8892777c8ff
eilmadc <eilmadc@gmail.com>
2021-06-23 23:43:10 +0200

    commit tab2

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@angular/core';
+
+import { Plugins } from '@capacitor/core';
+const { Geolocation } = Plugins;
+
 declare let google;
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class MapService {
   
   constructor() { }
 
-  async initMap(): void {
-    const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  async initMap(){
+    const map = await new google.maps.Map(document.getElementById('map'), {
       center: { lat: -34.397, lng: 150.644 },
       zoom: 8,
-    })
+    });
 }
+
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current', coordinates);
+  }
+
+  watchPosition() {
+    const wait = Geolocation.watchPosition({}, (position, err) => {});
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MapService } from '../services/map.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(
+    private map: MapService
+  ) { }
 
+  ngOnInit(): void{
+    this.map.initMap();
+    this.map.getCurrentPosition();
+  }
 }
